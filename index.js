13990374301f599bc202588c66ae0e5e9cbcb1ce
CristianDev <camilovargas_12@misena.edu.co>
2025-07-30 17:26:55 -0500

    CCVO: CAMBIANDO EL PORT

--- index.js
+++ index.js
@@ -108,6 +108,8 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log("Servidor Socket.io escuchando en http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`Servidor Socket.io escuchando en el puerto ${PORT}`);
 });
